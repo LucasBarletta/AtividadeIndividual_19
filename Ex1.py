61c8127e0b5193c8081777e0e434abb574967723
Lucas <lubarlettamunhoz@gmail.com>
2019-08-19 17:15:22 -0300

    Corrigir nome funcao

--- Ex1.py
+++ Ex1.py
@@ -1,14 +1,14 @@
 numero = [9,8,7,1,4,6,7,8,9]
-digitar = int(input('Digite 1 para par e 2 para impar: '))
+digitar = int(input('Digite 1 para impar e 2 para par: '))
 
-def impar(numero):
+def par(numero):
 
     for n in numero:
         if n % 2 == 0:
             print(n)
     return numero
 
-def par(numero):
+def impar(numero):
 
     for n in numero:
         if n % 2 != 0:
